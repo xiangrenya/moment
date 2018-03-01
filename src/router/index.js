6ed007a376196aa2c0d4ff2df8c12c8d9d2204f7
xiangrenya <xiangrenya@gmail.com>
2018-03-01 19:11:45 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -2,18 +2,24 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Moment from '@/views/Moment'
+import Login from '@/views/Login'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/hello',
       name: 'HelloWorld',
       component: HelloWorld
     },
     {
-      path: '/moment',
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/',
       name: 'Moment',
       component: Moment
     }
---
+++ src/views/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <form>
+      <div class="form-group">
+        <label for="account">用户名</label>
+        <input type="text" class="form-control"placeholder="Enter email">
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+      </div>
+      <div class="form-group">
+        <label for="password">密码</label>
+        <input type="password" class="form-control" placeholder="密码">
+      </div>
+      <div class="form-check">
+        <label class="form-check-label">
+          <input type="checkbox" class="form-check-input">
+          记住我
+        </label>
+      </div>
+      <button type="submit" class="btn btn-primary">登录</button>
+    </form>
+  </div>
+</template>
